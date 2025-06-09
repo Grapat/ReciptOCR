3fbc101cf1437584e082fa79361c3d97bce9527e
unknown <s6502041620026@email.kmutnb.ac.th>
2025-06-09 09:47:30 +0700

    proto

--- reciptOCRfrontend/src/App.jsx
+++ reciptOCRfrontend/src/App.jsx
@@ -147,30 +147,12 @@ function App() {
             accept="image/png, image/jpeg, image/jpg, image/gif, image/webp"
             onChange={handleImageChange}
           />
-          <label
+          <button
             htmlFor="imageUpload"
             className="file-input-button"
           >
-            {/* Lucide FileUp icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="file-input-icon"
-            >
-              <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-              <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-              <path d="M12 16V9" />
-              <path d="M18 14l-6-6-6 6" />
-            </svg>
             Choose Receipt Image
-          </label>
+          </button>
         </div>
 
         {/* Status Message */}
